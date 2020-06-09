b126ed7ceeafac8942765e7d43a4f4ebbfc4649e
ChenNing02 <2633565885@qq.com>
2020-06-09 17:48:44 +0800

    修改请求判断方式为onreadyStatechange

--- public/main.js
+++ public/main.js
@@ -40,14 +40,16 @@ getJS.onclick = ()=>{
 getHTML.onclick = ()=>{
     const request = new XMLHttpRequest()
     request.open('GET', '/3.html')
-    request.onload = ()=>{
-        console.log('请求成功')
-        const div = document.createElement('div')
-        div.innerHTML = request.response
-        document.body.appendChild(div)
-    }
-    request.onerror = ()=>{
-        console.log('请求失败')
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            if(request.status >= 200 && request.status < 300){
+                const div = document.createElement('div')
+                div.innerHTML = request.response
+                document.body.appendChild(div)
+            }else {
+                alert('加载div失败')
+            }
+        }
     }
     request.send()
 }
